32e2ff518c5a1f73f7552a453e42b2352a2c3608
Mikhail Volynov <st3althtech@mail.ru>
2019-02-03 11:21:32 +0300

    Org -> lowercase

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,2 @@
-docker tag node-web-app $registry_url/Sample/node-web-app:$CI_PIPELINE_ID
-docker push $registry_url:5000/Sample/node-web-app:$CI_PIPELINE_ID
+docker tag node-web-app $registry_url/sample/node-web-app:$CI_PIPELINE_ID
+docker push $registry_url:5000/sample/node-web-app:$CI_PIPELINE_ID
