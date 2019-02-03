a8212c631342d7dc7ba983f378cc8c8d686da672
Mikhail Volynov <st3althtech@mail.ru>
2019-02-03 11:12:21 +0300

    Added organization

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,2 @@
-docker tag node-web-app $registry_url/node-web-app:$CI_PIPELINE_ID
-docker push $registry_url:5000/node-web-app:$CI_PIPELINE_ID
+docker tag node-web-app $registry_url/Sample/node-web-app:$CI_PIPELINE_ID
+docker push $registry_url:5000/Sample/node-web-app:$CI_PIPELINE_ID
